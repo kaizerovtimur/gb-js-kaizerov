41fb778ebd97daa2a0fbe2a2d3890a14c177deae
kaizerovtimur <kaizerovtimur@gmail.com>
2019-04-29 00:01:33 +0300

    Main

---
+++ HW1/HW1.1/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let Tc = prompt("Введите температуру по цельсию:");
+
+let Tf = (9 / 5) * Tc + 32;
+
+alert(Tf);
---
+++ HW1/HW1.2/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let name;
+let admin;
+
+name = "Василий";
+
+admin = name;
+
+alert(admin);
